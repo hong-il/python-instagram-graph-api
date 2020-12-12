c9bba813161f5f8e3d1653066cde30b1958c8079
KIM HONGIL <clroyan01@gmail.com>
2020-12-12 23:50:51 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+    @ Author    : hong-il
+    @ Date      : 2020-12-12
+    @ File name : main.py
+    @ File path : 
+    @ Description : 
+"""
+
+
+def main():
+    print(f'Hi, PyCharm!')
+
+
+if __name__ == '__main__':
+    main()
