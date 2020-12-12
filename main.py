4c508262256dfa0f907da13cda2f73ef27c3c7a9
KIM HONGIL <clroyan01@gmail.com>
2020-12-13 01:01:41 +0900

    init 변형

--- main.py
+++ main.py
@@ -9,22 +9,18 @@
 class Instagram:
 
     def __init__(self, dictionary):
-        self.dict = dict
+        self.dictionary = dict()
+        self.dictionary['안준석'] = 172
 
     def run(self):
-        for d in self:
-            print(f'{d}')
+        for k, v in self.dictionary.items():
+            print(f'{k} : {v}')
 
 
 def main():
-    dictionary = [
-        {"이름": "안준석", "키": 170},
-        {"이름": "김홍일", "키": 162},
-        {"이름": "박상혁", "키": 155},
-        {"이름": "표중희", "키": 207},
-        {"이름": "박상언", "키": 177}
-    ]
-    Instagram.run(self=dictionary)
+    dictionary = {"이름": "안준석", "키": 178}
+    instagram = Instagram(dictionary=dictionary)
+    instagram.run()
 
 
 if __name__ == '__main__':
